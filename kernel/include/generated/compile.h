442cbb48b96529f1db1b65f1cdf4794e01637b6a
Dennis Boyce <dsb9938@yahoo.com>
2011-11-20 13:28:48 -0800

    Added Laggfree Governor

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 5 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 PREEMPT Sun Nov 20 09:51:07 PST 2011"
-#define LINUX_COMPILE_TIME "09:51:08"
+#define UTS_VERSION "#5 PREEMPT Sun Nov 20 12:09:41 PST 2011"
+#define LINUX_COMPILE_TIME "12:09:41"
 #define LINUX_COMPILE_BY "dsb9938"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILE_DOMAIN "(none)"
--- kernel/include/generated/utsrelease.h
+++ kernel/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "2.6.35.7"
+#define UTS_RELEASE "2.6.35.7-g183df40-dirty"
